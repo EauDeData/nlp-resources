64ced6e16b6c91e3c6aaaabbeb013b3d0022e299
EauDeData <amolina@cvc.uab.cat>
2023-02-28 15:03:38 +0100

    dataloader parser

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -1,4 +1,5 @@
 import os
+import random
 from typing import *
 
 class EsposallesTextDataset:
@@ -17,22 +18,37 @@ class EsposallesTextDataset:
         
     '''
 
-    def __init__(self, base_folder: str = 'esposalles', download: bool = True) -> None:
+    def __init__(self, base_folder: str = 'esposalles', download: bool = True, train_split = 0.9, shuffle = True) -> None:
         
         self.base_folder = base_folder
+        self.split = train_split
         if not 'gt' in os.listdir(self.base_folder) and download: self.download_data()
-        self.parsse_data()
-        print(self.data)
+        self.parse_data()
+        self.registers = list(self.data.keys())
+        self.train()
+        if shuffle: random.shuffle(self.registers)
     
     def __len__(self):
-        pass
+        if self.offset: return int(len(self.registers) * (1 - self.split))
+        return  int(len(self.registers) * self.split)
+
+    def test(self):
+        self.offset = int(len(self.data) * self.split)
+    
+    def train(self):
+        self.offset = 0
 
     def __iter__(self):
         self.inner_state = 0
         return self
 
     def __getitem__(self, idx: int) -> Tuple[List[str], List[int]]:
-        pass
+        
+        idx += self.offset
+        register = self.registers[idx]
+        x, gt =  self.data[register]
+        
+        return x.split(), [None if y == 'other' else y for y in gt.split()]
 
     def __next__(self):
         
@@ -45,7 +61,7 @@ class EsposallesTextDataset:
     def download_data(self):
         pass
 
-    def parsse_data(self):
+    def parse_data(self):
         
         get_stripped_lines = lambda x: ' '.join([x.strip().split(':')[-1] for x in open(x).readlines()])
 
@@ -63,4 +79,11 @@ class EsposallesTextDataset:
         pass
 
 
-EsposallesTextDataset()
+E = EsposallesTextDataset()
+print(E[0], len(E))
+E.test()
+
+print(E[0], len(E))
+E.train()
+
+print(E[0], len(E))
